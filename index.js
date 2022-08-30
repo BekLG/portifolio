8726a0872d4aeb9d088da18afa844a7000fef24b
bekLg <bereketlegesse22@gmail.com>
2022-08-30 05:50:49 +0300

    all is up to date

---
+++ index.js
@@ -0,0 +1,28 @@
+$("#btnNightDay").click(function(){
+
+    $(".navigation").toggleClass("dark1");
+    $(".main-div").toggleClass("dark2");
+    $(".name").toggleClass("dark2");
+    $(".intro-text").toggleClass("dark2");
+    $(".about-page").toggleClass("dark1");
+    $(".contact-page").toggleClass("dark1");
+    $(".svgpath").toggleClass("dark1");
+    $("#btn-download-cv").toggleClass("btn-outline-light btn-primary");
+    $("#contact-title").toggleClass("dark1");
+    $(".cont-methods").toggleClass("dark-item1");
+    $(".about-text-intro-name").toggleClass("dark-item2");
+    $("#btn-send").toggleClass("btn-outline-light");
+    $("#btn-reset").toggleClass("btn-outline-light");
+    
+    if($("#btnNightDay").hasClass("btn-dark"))
+    {
+        $("#btnNightDay").removeClass("btn-dark");
+        $("#btnNightDay").addClass("btn-light");
+    }
+    else if($("#btnNightDay").hasClass("btn-light"))
+    {
+        $("#btnNightDay").removeClass("btn-light");
+        $("#btnNightDay").addClass("btn-dark");
+    }
+
+});
