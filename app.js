760f6764e2fbb0bf827ab72e24b8290cf08cc3cd
BekLg <bereketlegesse22@gmail.com>
2023-04-07 07:31:53 +0300

    added project schema to database

--- app.js
+++ app.js
@@ -9,13 +9,30 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/myProjectsDB", {useNewUrlParser: true});
+
+
+const projectSchema= new mongoose.Schema({
+    title: String,
+    imageLink: String,
+    description: String,
+    detailedDescription: String,
+    githubLink: String,
+    demoLink: String
+});
+
+
+const project= new mongoose.model("Project", projectSchema);
+
 
 
 app.get("/", function(req,res){
     res.render("home")
 })
 
-
+app.get("/add-project", function(req,res){
+    res.render("addProject");
+})
 
 
 app.listen(3000, function() {
