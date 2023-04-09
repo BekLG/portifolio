bef4d44b2f163cdbebe77f7cb07758980292fd84
BekLg <bereketlegesse22@gmail.com>
2023-04-09 16:42:26 +0300

    able to delete project from the admin page

--- app.js
+++ app.js
@@ -30,21 +30,78 @@ app.get("/", function(req,res){
 
     Project.find({})
     .then((foundProjectArray)=>{
-
-        console.log(foundProjectArray.title);
         res.render("home", { foundProjects: foundProjectArray })
-       
     })
     .catch((err)=>{
         console.log(err);
     })
 
 })
+app.get("/admin", function(req,res){
+    Project.find({})
+    .then((foundProjectArray)=>{
+        res.render("adminPage", { foundProjects: foundProjectArray })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
 
-app.get("/add-project", function(req,res){
+app.post("/delete", function(req,res){
+    const projectId= req.body.projectId;
+
+    Project.findByIdAndDelete(projectId)
+    .then(() => {
+      console.log("project deleted successfully.");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+    res.redirect("/admin")
+
+})
+
+app.post("/editProject", function(req,res){ 
+
+    const projectId= req.body.projectId;
+
+    Project.findById(projectId)
+    .then((foundProject)=>{
+        const title= foundProject.title;
+        const imageLink= foundProject.imgLink;
+        const description=foundProject.shortDescription;
+        const detailedDescription=foundProject.detailedDescription;
+        const githubLink= foundProject.githubLink;
+        const demoLink= foundProject.demoLink;
+
+        res.render("editProject",
+        { 
+            projectId: projectId,
+            title: title,
+            imageLink: imageLink,
+            description: description,
+            detailedDescription: detailedDescription,
+            githubLink: githubLink,
+            demoLink: demoLink
+        })
+    
+        console.log(title);
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+   
+   
+  
+
+})
+
+app.get("/addProject", function(req,res){
     res.render("addProject");
 })
-app.post("/add-project", function(req,res){
+app.post("/addProject", function(req,res){
 
    const title= req.body.title;
    const imageLink= req.body.imgLink;
