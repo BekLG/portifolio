ccbb44620c8a4d1e2e2fc2f0175beb29731475ac
BekLg <bereketlegesse22@gmail.com>
2023-04-16 14:54:25 +0300

    changed directory of views in express

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const bcrypt= require("bcrypt");
 const app= express();
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname/views);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
