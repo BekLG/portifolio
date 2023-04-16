1e9af7b67d4afa72e904a38cbe84e8d0a0bcaeb2
BekLg <bereketlegesse22@gmail.com>
2023-04-16 17:45:13 +0300

    changed the views directory to root

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bcrypt= require("bcrypt");
 const app= express();
 
 app.set('view engine', 'ejs');
-app.set('views', '/var/task/views');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
