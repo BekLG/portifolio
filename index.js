30af43c383a044211d5058236dbdcc8c3a39a553
BekLg <bereketlegesse22@gmail.com>
2023-04-16 18:01:19 +0300

    added path for public folder too

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app= express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.set('public', path.join(__dirname, 'public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
