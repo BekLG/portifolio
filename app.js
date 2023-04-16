2ac25f9a6f20f1b9ffbdc7b8c34256dbfcb3e3c5
bek <54703839+BekLG@users.noreply.github.com>
2023-04-16 12:22:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -226,7 +226,7 @@ app.post("/:projectTitle", function(req,res){
     })
 })
 
-app.listen(3000, function(req,res) {
+app.listen(process.env.PORT, function(req,res) {
     console.log("Server started on port 3000.");
     Admin.find({email: process.env.EMAIL_1})
     .then((foundAdmin)=>{
